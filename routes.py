bdef4312158d6ce8df1f7e56ca65bd32ab635d39
Khaled Allen <khaled.allen@gmail.com>
2020-02-19 17:36:18 -0700

    moved the router for the root to look at the index template

--- routes.py
+++ routes.py
@@ -3,7 +3,7 @@ from flask import render_template, url_for
 
 @app.route('/')
 def main():
-    return render_template('base.html')
+    return render_template('index.html')
 
 @app.route('/index')
 def index():
